123a9d0a61584d7fd2cba4983ff8539f44326e1f
user-b <user-b@localhost>
2025-07-06 00:41:34 +0900

    udp_coreクラスにポートメンバーを追加し、UDPクラスの構造を整理

--- include/udp.h
+++ include/udp.h
@@ -13,12 +13,26 @@ namespace unet
         int send_m(struct sockaddr_in *addr, const char *buf, int len);
         int recv_m(struct sockaddr_in *addr, char *buf, int len);
         int sock = 0;
+        int port = 0;
 
     public:
         udp_core();
         ~udp_core();
     };
 
+    class UDP : public udp_core
+    {
+    public:
+        UDP();
+        UDP(int port);
+        ~UDP();
+
+        int set_port(int port);
+        int send_data(const char *addr_, const char *buf, int len);
+        int recv_data(char *buf, int len);
+        int recv_data(struct sockaddr_in *addr, char *buf, int len);
+        void close_s();
+    };
 }
 
 #endif // UDP_H
