7e0877fc0dc0f7fee086155359fb7e169f5db475
user-b <user-b@localhost>
2026-02-22 09:48:30 +0900

    変数名改善

--- include/udp.h
+++ include/udp.h
@@ -12,18 +12,39 @@ namespace unet
     protected:
         int send_m(const IPaddress *addr, const char *buf, int len) const noexcept;
         int recv_m(IPaddress *addr, char *buf, int len) const noexcept;
-        mutable int Tsock = 0;
-        int Tport = 0;
-        mutable int Rsock = 0;
-        int Rport = 0;
+        mutable int TXsock = 0;
+        int TXport = 0;
+        mutable int RXsock = 0;
+        int RXport = 0;
 
     public:
+        /// @brief UDPクラス
+        /// @param Tx_ 送信ポート
+        /// @param Rx_ 受信ポート
         udp_core(int Tx_, int Rx_);
         udp_core();
         ~udp_core();
+        /// @brief ポート設定
+        /// @param Tx_ 送信ポート
+        /// @param Rx_ 受信ポート
+        /// @return 0で成功-1で失敗
         int set_port(int Tx_, int Rx_);
+        /// @brief データ送信
+        /// @param addr_ 宛先アドレス（ドメイン可）
+        /// @param buf データバッファ
+        /// @param len 送信長
+        /// @return 0で成功-1で失敗
         int send_data(const char *addr_, const char *buf, int len);
+        /// @brief データ受信
+        /// @param buf 受信バッファ
+        /// @param len 受信長
+        /// @return 0で成功-1で失敗
         int recv_data(char *buf, int len);
+        /// @brief データ受信（送信元付き）
+        /// @param addr 送信元アドレス
+        /// @param buf 受信バッファ
+        /// @param len 受信長
+        /// @return 0で成功-1で失敗
         int recv_data(IPaddress *addr, char *buf, int len);
     };
 
--- src/udp_core.cpp
+++ src/udp_core.cpp
@@ -7,8 +7,8 @@ namespace unet
 
     int udp_core::send_m(const IPaddress *addr, const char *buf, int len) const noexcept
     {
-        Tsock = socket(addr->ss_family, SOCK_DGRAM, 0);
-        if (Tsock < 0)
+        TXsock = socket(addr->ss_family, SOCK_DGRAM, 0);
+        if (TXsock < 0)
         {
             perror("udp send socket()");
             return error;
@@ -21,15 +21,15 @@ namespace unet
         else
             addrlen = sizeof(struct sockaddr_storage);
 
-        int ret = sendto(Tsock, buf, len, 0, (struct sockaddr *)addr, addrlen);
-        close(Tsock);
-        Tsock = 0;
+        int ret = sendto(TXsock, buf, len, 0, (struct sockaddr *)addr, addrlen);
+        close(TXsock);
+        TXsock = 0;
         return ret;
     }
     int udp_core::recv_m(IPaddress *addr, char *buf, int len) const noexcept
     {
         socklen_t addr_len = sizeof(IPaddress);
-        int ret = recvfrom(Rsock, buf, len, 0, (struct sockaddr *)addr, &addr_len);
+        int ret = recvfrom(RXsock, buf, len, 0, (struct sockaddr *)addr, &addr_len);
         return ret;
     }
     udp_core::udp_core(int Tx_, int Rx_)
@@ -39,51 +39,51 @@ namespace unet
         {
             return; //
         }
-        Tport = Tx_;
-        Rport = Rx_;
-        Rsock = socket(AF_INET6, SOCK_DGRAM, 0);
-        if (Rsock < 0)
+        TXport = Tx_;
+        RXport = Rx_;
+        RXsock = socket(AF_INET6, SOCK_DGRAM, 0);
+        if (RXsock < 0)
         {
-            perror("Error. Cannot make Rsock");
-            Rsock = 0;
+            perror("Error. Cannot make RXsock");
+            RXsock = 0;
             return;
         }
         int off = 0;
-        if (setsockopt(Rsock, IPPROTO_IPV6, IPV6_V6ONLY,
+        if (setsockopt(RXsock, IPPROTO_IPV6, IPV6_V6ONLY,
                        (char *)&off, sizeof(off)) < 0)
         {
             perror("setsockopt IPV6_V6ONLY");
         }
         IPaddress addr_ = {0};
         addr_.ss_family = AF_INET6;
-        ((struct sockaddr_in6 *)&addr_)->sin6_port = htons(Rport);
+        ((struct sockaddr_in6 *)&addr_)->sin6_port = htons(RXport);
         ((struct sockaddr_in6 *)&addr_)->sin6_addr = in6addr_any;
-        if (bind(Rsock, (struct sockaddr *)&addr_, sizeof(addr_)) < 0)
+        if (bind(RXsock, (struct sockaddr *)&addr_, sizeof(addr_)) < 0)
         {
-            perror("bind Rsock failed");
-            close(Rsock);
-            Rsock = 0;
+            perror("bind RXsock failed");
+            close(RXsock);
+            RXsock = 0;
             return;
         }
 
 #ifndef NETCPP_BLOCKING
         u_long val = 1;
-        ioctl(Rsock, FIONBIO, &val);
+        ioctl(RXsock, FIONBIO, &val);
 #endif // NETCPP_BLOCKING
         udp_no++;
     }
     udp_core::udp_core()
     {
         netcpp_start();
-        Rsock = socket(AF_INET6, SOCK_DGRAM, 0);
-        if (Rsock < 0)
+        RXsock = socket(AF_INET6, SOCK_DGRAM, 0);
+        if (RXsock < 0)
         {
-            perror("Error. Cannot make Rsock");
-            Rsock = 0;
+            perror("Error. Cannot make RXsock");
+            RXsock = 0;
             return;
         }
         int off = 0;
-        if (setsockopt(Rsock, IPPROTO_IPV6, IPV6_V6ONLY,
+        if (setsockopt(RXsock, IPPROTO_IPV6, IPV6_V6ONLY,
                        (char *)&off, sizeof(off)) < 0)
         {
             perror("setsockopt IPV6_V6ONLY");
@@ -91,24 +91,24 @@ namespace unet
 
 #ifndef NETCPP_BLOCKING
         u_long val = 1;
-        ioctl(Rsock, FIONBIO, &val);
+        ioctl(RXsock, FIONBIO, &val);
 #endif // NETCPP_BLOCKING
         udp_no++;
     }
 
     udp_core::~udp_core()
     {
-        if (Tsock > 0)
+        if (TXsock > 0)
         {
-            shutdown(Tsock, SHUT_RW);
-            close(Tsock);
-            Tsock = 0;
+            shutdown(TXsock, SHUT_RW);
+            close(TXsock);
+            TXsock = 0;
         }
-        if (Rsock > 0)
+        if (RXsock > 0)
         {
-            shutdown(Rsock, SHUT_RW);
-            close(Rsock);
-            Rsock = 0;
+            shutdown(RXsock, SHUT_RW);
+            close(RXsock);
+            RXsock = 0;
         }
         udp_no--;
         netcpp_stop();
@@ -119,15 +119,15 @@ namespace unet
         {
             return -1; //
         }
-        Tport = Tx_;
-        Rport = Rx_;
+        TXport = Tx_;
+        RXport = Rx_;
         IPaddress addr_ = {0};
         addr_.ss_family = AF_INET6;
-        ((struct sockaddr_in6 *)&addr_)->sin6_port = htons(Rport);
+        ((struct sockaddr_in6 *)&addr_)->sin6_port = htons(RXport);
         ((struct sockaddr_in6 *)&addr_)->sin6_addr = in6addr_any;
-        if (bind(Rsock, (struct sockaddr *)&addr_, sizeof(addr_)) < 0)
+        if (bind(RXsock, (struct sockaddr *)&addr_, sizeof(addr_)) < 0)
         {
-            perror("bind Rsock failed in set_port");
+            perror("bind RXsock failed in set_port");
             return error;
         }
         return success;
@@ -136,7 +136,7 @@ namespace unet
     int udp_core::send_data(const char *addr, const char *buf, int len)
     {
         IPaddress addr_in = {0};
-        getipaddrinfo(addr, Tport, addr_in, UDP_c);
+        getipaddrinfo(addr, TXport, addr_in, UDP_c);
         return send_m(&addr_in, buf, len);
     }
 
